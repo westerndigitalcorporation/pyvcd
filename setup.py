ff10d84f9235407a006bdb462ba47d386f6b1f3d
Pete Grayson <pete@jpgrayson.net>
2018-11-21 16:33:36 -0500

    Travis config updates
    Add Python 3.7, 3.8, and nightly to test matrix. Remove 3.3 which is no
    longer supported by setuptools.
    
    Only run coverage/coveralls on one 3.x and one 2.x variant.
    
    Remove 'sudo = false'. This is done to align with the Travis-CI service
    migrating to all VM environments (i.e. instead of docker containers).
    
    Update classifiers in setup.py to include Python 3.7 and remove 3.3.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 from setuptools import setup
 
-
 setup(
     name='pyvcd',
     author='Peter Grayson',
@@ -31,10 +30,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Scientific/Engineering',
